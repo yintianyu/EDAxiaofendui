eae293c5422fac5a023d0f0c659d1d21f3629392
yintianyu <eecsyty@outlook.com>
2019-10-17 16:20:53 +0800

    Add Homo_Regulation.hpp（均匀量化）

---
+++ Homo_Regulation.hpp
@@ -0,0 +1,24 @@
+/*
+ * File: Homo_Regulation.hpp
+ * Author: Tianyu Yin
+ * Create Date: 2019/10/06
+ * Description: Define class Homo_Regulation
+ */
+
+#ifndef HOMO_REGULATION_HPP
+#define HOMO_REGULATION_HPP
+
+#include "Regulation.hpp"
+
+class Homo_Regulation : public Regulation{
+    public:
+	static int pieceStarts[8] ;
+	static int minInterval[8] ;
+    Homo_Regulation(){}
+	~Homo_Regulation(){}
+    void compress(const std::vector<original_data> &input_diff, original_data max, std::vector<compressed_diff> &output_compressed_diff);
+    void decompress(const std::vector<compressed_diff> &input_diff, original_data max, std::vector<original_data> &output_original_data);
+    
+};
+
+#endif
