53388634a99c36bbcea56004d558682f26dbdbeb
spike556 <spikexin@outlook.com>
2019-11-13 16:14:02 +0800

    1

--- u_Regulation.cpp
+++ u_Regulation.cpp
@@ -54,28 +54,28 @@ void u_Regulation :: decompress(const std::vector<compressed_diff> &input_diff, 
         auto num = j.to_ulong();
         double sig = (*i)[s-1] == 1 ? -1 : 1; 
         if (num >= maxVal*7/8) {
-            res = (127.0 / 255 + (original_data) (num - maxVal*7/8) * 1024 / (255*maxVal)) * max;
+            res = (127.0 / 255 + (original_data) (num + 0.5 - maxVal*7/8) * 1024 / (255*maxVal)) * max;
         } 
         else if (num >=  maxVal*6/8) {
-            res = (63.0 / 255 + (original_data) (num - maxVal*6/8) * 512 / (255*maxVal)) * max;
+            res = (63.0 / 255 + (original_data) (num + 0.5 - maxVal*6/8) * 512 / (255*maxVal)) * max;
         } 
         else if (num >= maxVal*5/8) {
-            res = (31.0 / 255 + (original_data) (num - maxVal*5/8) * 256 / (255*maxVal)) * max;
+            res = (31.0 / 255 + (original_data) (num + 0.5 - maxVal*5/8) * 256 / (255*maxVal)) * max;
         }
         else if (num >= maxVal*4/8) {
-            res = (15.0 / 255 + (original_data) (num - maxVal*4/8) * 128 / (255*maxVal)) * max;
+            res = (15.0 / 255 + (original_data) (num + 0.5 - maxVal*4/8) * 128 / (255*maxVal)) * max;
         }
         else if (num >= maxVal*3/8) {
-            res = (7.0 / 255 + (original_data) (num - maxVal*3/8) * 64 / (255*maxVal)) * max;
+            res = (7.0 / 255 + (original_data) (num + 0.5 - maxVal*3/8) * 64 / (255*maxVal)) * max;
         }
         else if (num >=  maxVal*2/8) {
-            res = (3.0 / 255 + (original_data) (num - maxVal*2/8) * 32 / (255*maxVal)) * max;
+            res = (3.0 / 255 + (original_data) (num + 0.5 - maxVal*2/8) * 32 / (255*maxVal)) * max;
         }
         else if (num >= maxVal*1/8) {
-            res = (1.0 / 255 + (original_data) (num - maxVal*1/8) * 16 / (255*maxVal)) * max;
+            res = (1.0 / 255 + (original_data) (num + 0.5 - maxVal*1/8) * 16 / (255*maxVal)) * max;
         }
         else {
-            res = max * (original_data) num * 8 / (255*maxVal);
+            res = max * (original_data) (num + 0.5) * 8 / (255*maxVal);
         }
         res = res * sig;
         output_original_data.push_back(res);
